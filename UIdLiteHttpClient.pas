8fabf893b01e692c127d117090eceaabfb9fb4cf
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2023-02-18 19:06:41 -0300

    First release

--- UIdLiteHttpClient.pas
+++ UIdLiteHttpClient.pas
@@ -20,6 +20,7 @@ type
 
   TIdLhcResponse = record
     StatusCode    : Word;
+    ReasonPhrase  : String;
     ContentType   : String;
     ContentLength : Integer;
     ContentStream : TMemoryStream;
@@ -28,8 +29,8 @@ type
 
   TIdLiteHttpClient = class(TObject)
     private
-      FSocket        : TIdTCPClient;
-      FTimeout       : Integer;
+      FClient        : TIdTCPClient;
+      FTimeout       : Word;
 
       FHost          : String;
       FPort          : Word;
@@ -38,8 +39,10 @@ type
       FPassword      : String;
       FAuthorization : String;
 
+      procedure   SetTimeout(ATimeout: Word = 20);
+
       function    GetURLSegments(const AUrlOrPath: String)                                                      : TIdLhcURL;
-      function    BuildRequest(AMethod, AUrl, AData, AContentType: String)                                      : String;
+      function    BuildRequest(AMethod, AHostName, APath, AData, AContentType: String)                          : String;
 
       function    Request(AMethod, AUrlOrPath: String; AData: String = ''; AContentType: String = 'text/plain') : TIdLhcResponse;
     public
@@ -48,13 +51,13 @@ type
 
       procedure   SetAuthentication(AUsername, APassword: String);
 
-      function    Get(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')        : TIdLhcResponse;
-      function    Put(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')        : TIdLhcResponse;
-      function    Post(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')       : TIdLhcResponse;
-      function    Patch(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')      : TIdLhcResponse;
-      function    Delete(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')     : TIdLhcResponse;
+      function    Get(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')    : TIdLhcResponse;
+      function    Put(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')    : TIdLhcResponse;
+      function    Post(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')   : TIdLhcResponse;
+      function    Patch(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json')  : TIdLhcResponse;
+      function    Delete(AUrlOrPath: String; AData: String = ''; AContentType: String = 'application/json') : TIdLhcResponse;
 
-      property    Timeout : Integer read FTimeout write FTimeout default 20000;
+      property    Timeout : Word read FTimeout write SetTimeout default 20;  // In seconds
   end;
 
 implementation
@@ -64,7 +67,7 @@ uses
 
 { TIdLiteHttpClient }
 
-function TIdLiteHttpClient.BuildRequest(AMethod, AUrl, AData, AContentType: String): String;
+function TIdLiteHttpClient.BuildRequest(AMethod, AHostName, APath, AData, AContentType: String): String;
 { GetMimeType }
 function GetMimeType(const AData: String): String;
 var
@@ -86,25 +89,18 @@ begin
   end;
 end;
 { BuildRequest }
-const
-  CRLF=#13#10;
-var
-  LURL  : TIdLhcURL;
 begin
-  LURL         := GetURLSegments(AUrl);
-  AContentType := IfThen((Length(AData) > 0) and (Trim(AContentType) = ''),GetMimeType(AData),Trim(AContentType));
-
-  Result       := IfThen(AMethod = '','GET',UpperCase(AMethod)) + ' ' + LURL.Path + ' HTTP/1.1' + CRLF;
-  Result       := Result + 'Host: ' + LURL.HostName + CRLF;
-  if (FAuthorization <> '') then Result := Result + 'Authorization: Basic ' + FAuthorization + CRLF;
-  Result       := Result + 'Connection: close' + CRLF;
+  Result       := IfThen(AMethod = '','GET',UpperCase(AMethod)) + ' ' + IfThen(Pos('/',APath) = 0,'/') + APath + ' HTTP/1.1' + sLineBreak;
+  if (AHostName <> '')      then Result := Result + 'Host: ' + AHostName + sLineBreak;
+  if (FAuthorization <> '') then Result := Result + 'Authorization: Basic ' + FAuthorization + sLineBreak;
+  Result       := Result + 'Connection: close' + sLineBreak;
 
   if (not (AData = '')) then begin
-    Result := Result + 'Content-Type: ' + AContentType + CRLF;
-    Result := Result + 'Content-Length: ' + IntToStr(Length(AData)) + CRLF ;
-    Result := Result + CRLF;
+    Result := Result + 'Content-Type: ' + IfThen((Length(AData) > 0) and (Trim(AContentType) = ''),GetMimeType(AData),Trim(AContentType)) + sLineBreak;
+    Result := Result + 'Content-Length: ' + IntToStr(Length(AData)) + sLineBreak;
+    Result := Result + sLineBreak;
     Result := Result + AData;
-  end else Result := Result + CRLF;
+  end else Result := Result + sLineBreak;
 end;
 
 constructor TIdLiteHttpClient.Create(const AHost: String; APort: Word);
@@ -112,20 +108,18 @@ begin
   FHost                  := AHost;
   FPort                  := APort;
 
-  FTimeout               := 20000;
-
   FUsername              := '';
   FPassword              := '';
   FAuthorization         := '';
 
-  FSocket                := TIdTCPClient.Create(nil);
-  FSocket.ConnectTimeout := FTimeout;
-  FSocket.ReadTimeout    := FTimeout;
+  FClient                := TIdTCPClient.Create(nil);
+
+  SetTimeout();
 end;
 
 destructor TIdLiteHttpClient.Destroy;
 begin
-  if Assigned(FSocket) then FreeAndNil(FSocket);
+  if Assigned(FClient) then FreeAndNil(FClient);
 
   inherited;
 end;
@@ -258,9 +252,86 @@ end;
 
 function TIdLiteHttpClient.Request(AMethod, AUrlOrPath, AData, AContentType: String): TIdLhcResponse;
 var
-  LRequest: String;
+  LURL            : TIdLhcURL;
+  LRequest, LLine : String;
+  LTries          : Word;
+
+  LStringStream   : TStringStream;
 begin
-  LRequest := BuildRequest(AMethod,AUrlOrPath,AData,AContentType);
+  FillChar(Result,SizeOf(Result),0);
+
+  LURL     := GetURLSegments(AUrlOrPath);
+
+  if LURL.HostName = '' then begin
+    raise Exception.Create('Invalid hostname');
+    Exit;
+  end;
+
+  if LURL.Port = 0 then begin
+    raise Exception.Create('Invalid port');
+    Exit;
+  end;
+
+  LRequest := BuildRequest(AMethod,LURL.HostName,LURL.Path,AData,AContentType);
+
+  if LRequest = '' then begin
+    raise Exception.Create('Invalid request');
+    Exit;
+  end;
+
+  while FClient.Connected do begin
+    FClient.Disconnect;
+    Sleep(100);
+  end;
+
+  try
+    FClient.Host           := LURL.HostName;
+    FClient.Port           := LURL.Port;
+
+    LStringStream          := TStringStream.Create('');
+
+    Result.ContentStream   := TMemoryStream.Create;
+    Result.ContentStream.SetSize(0);
+
+    try
+      FClient.Connect;
+
+      for LTries := FTimeout downto 1 do if FClient.Connected then break else Sleep(100);
+
+      if FClient.Connected then begin
+        FClient.Socket.Write(LRequest);
+
+        LLine := FClient.IOHandler.ReadLn;
+
+        if StartsText('HTTP',LLine) then begin
+          LLine               := Trim(ReplaceText(ReplaceText(ReplaceText(LLine,'HTTP/',''),'1.1',''),'1.0',''));
+          Result.StatusCode   := StrToIntDef(Trim(Copy(LLine,0,Pos(' ',LLine))),0);
+          Result.ReasonPhrase := Trim(Copy(LLine,Pos(' ',LLine)));
+
+          if Result.ReasonPhrase = '' then Result.ReasonPhrase := 'Unknown';          
+        end;
+
+        while LLine <> '' do begin
+          LLine := FClient.IOHandler.ReadLn;
+
+          if StartsText('Content-Length',LLine)    then Result.ContentLength := StrToIntDef(Trim(Copy(LLine,Pos(':',LLine) + 1,Length(LLine))),0);
+          if StartsText('Content-Type',LLine)      then Result.ContentType   := Trim(Copy(LLine,Pos(':',LLine) + 1,Length(LLine)));
+          if StartsText('Transfer-Encoding',LLine) then if SameText('chunked',Trim(Copy(LLine,Pos(':',LLine) + 1,Length(LLine)))) then Result.ContentLength := -1;
+        end;
+
+        if Result.ContentLength <> 0     then FClient.IOHandler.ReadStream(Result.ContentStream,Result.ContentLength,True);
+        if Result.ContentStream.Size > 0 then begin
+          LStringStream.CopyFrom(Result.ContentStream,0);
+          Result.ContentString := Trim(LStringStream.DataString);
+        end;
+      end;
+    except on E:Exception do
+      raise Exception.Create(E.Message);
+    end;
+  finally
+    if Assigned(LStringStream) then FreeAndNil(LStringStream);
+    if FClient.Connected       then FClient.DisconnectNotifyPeer;
+  end;
 end;
 
 procedure TIdLiteHttpClient.SetAuthentication(AUsername, APassword: String);
@@ -270,6 +341,17 @@ begin
   FUsername      := Trim(AUsername);
   FPassword      := Trim(APassword);
   FAuthorization := IfThen((FUsername <> '') and (FPassword <> ''),EncodeString(Concat(FUsername,SEP,FPassword)),'');
+end;
+
+procedure TIdLiteHttpClient.SetTimeout(ATimeout: Word);
+const
+  MIN_TIMEOUT=5;
+begin
+  if not Assigned(FClient)  then Exit;
+  if ATimeout < MIN_TIMEOUT then FTimeout := MIN_TIMEOUT else FTimeout := ATimeout;
+
+  FClient.ConnectTimeout := FTimeout * 1000;
+  FClient.ReadTimeout    := FClient.ConnectTimeout;
 end;
 
 end.
